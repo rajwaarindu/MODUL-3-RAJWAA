cedadb2bd578e71ed01b9b9906718640a2c48563
rajwaarindu <rajwaarindu@gmail.com>
2024-03-25 09:29:28 +0700

    first commit

---
+++ src/bangunruang/modul3bangunRuang.java
@@ -0,0 +1,23 @@
+package bangunruang;
+
+public class modul3bangunRuang {
+    public class App {
+        public static void main(String[] args) throws Exception {
+            Tabung tabung = new Tabung("tabung");
+            Kubus kubus = new Kubus("kubus");
+            Balok balok = new Balok("balok");
+
+            balok.inputNilai();
+            balok.luasPermukaan();
+            balok.volume();
+
+            kubus.inputNilai();
+            kubus.luasPermukaan();
+            kubus.volume();
+
+            tabung.inputNilai();
+            tabung.luasPermukaan();
+            tabung.volume();
+        }
+    }
+}
